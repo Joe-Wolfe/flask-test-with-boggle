3679f07d8593187fc46fa763b13b3f220ee82165
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-10-13 18:20:45 -0400

    Program can now check for valid answers

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
 from boggle import Boggle
 
 
@@ -17,3 +17,23 @@ def home():
     highscore = session.get("highscore", 0)
     numplays = session.get("numplays", 0)
     return render_template("index.html", board=board, highscore=highscore, numplays=numplays)
+
+
+@app.route("/check")
+def check():
+    """Checks the given word to make sure it is valid
+    Avalid word must
+    1. Be a valid word found in words.txt
+    2. Be a valid move on the boggle board.  
+
+    returns json dictionaries {“result”: “ok”} , {“result”: “not-on-board”}, or {“result”: “not-a-word”} 
+
+    """
+
+    guess = request.args["word"]
+    board = session["board"]
+
+    print(guess)
+    response = boggle_game.check_valid_word(board, guess)
+
+    return jsonify({"result": response})
--- static/boggle.js
+++ static/boggle.js
@@ -8,8 +8,22 @@ class BoggleGame {
     async handleSubmit(evt) {
         evt.preventDefault();
         let word = $("#user-guess").val();
-        console.log(word);
 
         $("#user-guess").val("").focus();
+        const response = await axios.get("/check", {
+            params: {
+                word: word
+            }
+        });
+        switch (response.data.result) {
+            case ("not-word"): this.showMessage(word + " is not a valid Enlish word."); break;
+            case ("not-on-board"): this.showMessage(word + " is not a playable word."); break;
+            default:
+                this.showMessage("congrats you found " + word) + "!";
+        }
+    }
+
+    showMessage(msg) {
+        $("#msg").text(msg)
     }
 }
