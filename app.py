3a94828580e334cc6277df750117c1c9c913ffcc
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-10-14 19:02:31 -0400

    finished base game. todo prevent duplicates, and add tests

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
     board = boggle_game.make_board()
     session['board'] = board
     highscore = session.get("highscore", 0)
-    numplays = session.get("numplays", 0)
+    numplays = session.get("numplays", 1)
     return render_template("index.html", board=board, highscore=highscore, numplays=numplays)
 
 
@@ -37,3 +37,17 @@ def check():
     response = boggle_game.check_valid_word(board, guess)
 
     return jsonify({"result": response})
+
+
+@app.route("/score", methods=["POST"])
+def post_score():
+    """sends a post request to the servers  and checks if a new record has been set and updates times played"""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    numplays = session.get("numplays", 0)
+
+    session["numplays"] = numplays+1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(newRecord=score > highscore)
--- static/boggle.js
+++ static/boggle.js
@@ -1,12 +1,18 @@
 class BoggleGame {
     constructor() {
         this.board = $("#game");
+        this.score = 0;
         $(".guesses", this.board).on("submit", this.handleSubmit.bind(this));
+
+        this.seconds = 60;
+        this.timer = setInterval(this.countdown.bind(this), 1000);
 
     }
 
     async handleSubmit(evt) {
         evt.preventDefault();
+
+        if (this.seconds === 0) return;
         let word = $("#user-guess").val();
 
         $("#user-guess").val("").focus();
@@ -19,11 +25,44 @@ class BoggleGame {
             case ("not-word"): this.showMessage(word + " is not a valid Enlish word."); break;
             case ("not-on-board"): this.showMessage(word + " is not a playable word."); break;
             default:
-                this.showMessage("congrats you found " + word) + "!";
+                this.showMessage("congrats you found '" + word) + "'!";
+                this.score += word.length;
+                this.updateScore();
         }
     }
 
     showMessage(msg) {
         $("#msg").text(msg)
+    }
+
+    updateScore() {
+        console.log(this.score)
+        $("#score").text(this.score);
+    }
+
+    updateTimer() {
+        $("#timer").text(this.seconds);
+
+    }
+
+    async countdown() {
+        console.log(this.seconds)
+        this.seconds -= 1;
+        this.updateTimer();
+
+        if (this.seconds === 0) {
+            clearInterval(this.timer);
+            await this.gameOver()
+        }
+    }
+
+    async gameOver() {
+        $("#user-guess").prop("disabled", true).attr("placeholder", "Time is up!");
+        const response = await axios.post("/score", { score: this.score });
+        if (response.data.newRecord === true) {
+            this.showMessage(`New record: ${this.score}`);
+        } else {
+            this.showMessage(`Final score: ${this.score}`);
+        }
     }
 }
